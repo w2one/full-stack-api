92baab2f429e57f801cfd36c648e335040d863a6
Jay <jay.wang.scorpion@gmail.com>
2019-08-06 13:22:05 +0800

    wechat

--- src/service/wechat/oAuth.js
+++ src/service/wechat/oAuth.js
@@ -9,7 +9,7 @@ import config from "../../config";
  * 根据code换取网页授权access_token
  * @param {token} token
  */
-const requestOauth2Access_token = async code =>
+export const requestOauth2Access_token = async code =>
   new Promise(function(resolve, reject) {
     let url =
       "https://api.weixin.qq.com/sns/oauth2/access_token?appid=APPID&secret=SECRET&code=CODE&grant_type=authorization_code";
@@ -42,7 +42,7 @@ const requestOauth2Access_token = async code =>
  * 根据openId获取微信信息
  * @param {token} token
  */
-const requestAuthUserinfo = async (ACCESS_TOKEN, OPENID) =>
+export const requestAuthUserinfo = async (ACCESS_TOKEN, OPENID) =>
   new Promise(function(resolve, reject) {
     let url =
       "https://api.weixin.qq.com/sns/userinfo?access_token=ACCESS_TOKEN&openid=OPENID&lang=zh_CN";
@@ -68,4 +68,4 @@ const requestAuthUserinfo = async (ACCESS_TOKEN, OPENID) =>
     );
   });
 
-export default { requestOauth2Access_token, requestAuthUserinfo };
+// export default { requestOauth2Access_token, requestAuthUserinfo };
